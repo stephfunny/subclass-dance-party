ad6a70efe18179711bf3f83734b5a0bc2b9a4df2
stephfunny <contactstephanief@gmail.com>
2018-01-22 19:40:41 -0800

    updated dancer and blinking dancer to use pseudoclassical instantiation, updated CSS

--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,42 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
-
-  var dancer = {};
-
-  // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
-    // the basic dancer doesn't do anything interesting at all on each step,
-    // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
-  };
-  dancer.step();
-
-  dancer.setPosition = function(top, left) {
-    // Use css top and left properties to position our <span> tag
-    // where it belongs on the page. See http://api.jquery.com/css/
-    //
-    var styleSettings = {
-      top: top,
-      left: left
-    };
-    dancer.$node.css(styleSettings);
-  };
-
+  this.$node = $('<span class="dancer"></span>');
+  this.timeBetweenSteps = timeBetweenSteps;
+  //timeBetweenSteps;
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  //this.top = top;
+  //this.left = left;
 
-  return dancer;
+  
+  // use jQuery to create an HTML <span> tag
+  this.left = left;
+  this.top = top;
+  this.step();
+  this.setPosition(top, left);
+  
 };
+
+
+makeDancer.prototype.step = function() {
+  console.log('dancers step this: ', this);
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+};
+
+
+makeDancer.prototype.setPosition = function(top, left) {
+  console.log('Top and left: ', top, left);
+  console.log('Dancers set pos: ', this);
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+  //
+  var styleSettings = {
+    top: top,
+    left: left
+  };
+  this.$node.css(styleSettings);
+};
+
+//dancer.setPosition(top, left);
--- src/init.js
+++ src/init.js
@@ -1,20 +1,10 @@
 $(document).ready(function() {
   window.dancers = [];
 
-  $('.addDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-     * buttons on dancefloor.html. You should only need to make one small change to it.
-     * As long as the "data-dancer-maker-function-name" attribute of a
-     * class="addDancerButton" DOM node matches one of the names of the
-     * maker functions available in the global scope, clicking that node
-     * will call the function to make the dancer.
-     */
+  $('body').css('background-image', 
+  'url("https://www.pitara.com/wordpress/wp-content/uploads/2003/09/what-is-a-volcano.jpg")');
 
-    /* dancerMakerFunctionName is a string which must match
-     * one of the dancer maker functions available in global scope.
-     * A new object of the given type will be created and added
-     * to the stage.
-     */
+  $('.addDancerButton').on('click', function(event) {
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
@@ -22,12 +12,81 @@ $(document).ready(function() {
 
     // make a dancer with a random position
 
-    var dancer = dancerMakerFunction(
+    var dancer = new dancerMakerFunction(
       $("body").height() * Math.random(),
       $("body").width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
   });
+
+
+  $('.addBadDancerButton').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
+    var dancerMakerFunction = window[dancerMakerFunctionName];
+
+    var dancer = new dancerMakerFunction(
+      $("body").height() * Math.random(),
+      $("body").width() * Math.random(),
+      Math.random() * 1000
+    );
+    $('body').append(dancer.$node);
+    window.dancers.push(dancer);
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* This function sets up the click handlers for the create-dancer
+ * buttons on dancefloor.html. You should only need to make one small change to it.
+ * As long as the "data-dancer-maker-function-name" attribute of a
+ * class="addDancerButton" DOM node matches one of the names of the
+ * maker functions available in the global scope, clicking that node
+ * will call the function to make the dancer.
+ */
+
+/* dancerMakerFunctionName is a string which must match
+ * one of the dancer maker functions available in global scope.
+ * A new object of the given type will be created and added
+ * to the stage.
+ */
